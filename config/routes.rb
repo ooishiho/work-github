e8c17d53a5cc3b1c5404bbfde8e168f2f419ea24
ooishiho <yuidacheng697@gmail.com>
2023-03-27 14:34:52 +0000

    [Add]管理者側view

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  namespace :admin do
-    resources :genres, only: [:index,:create,:edit,:update]
-  end
-  namespace :admin do
-    resources :items, only: [:new,:index,:create,:show,:edit,:update]
-  end
 # 顧客用
 devise_for :customers,skip: [:passwords],controllers: {
   registrations: "public/registrations",
@@ -14,6 +8,22 @@ devise_for :customers,skip: [:passwords],controllers: {
 devise_for :admin,skip: [:registrations, :passwords] , controllers: {
   sessions: "admin/sessions"
 }
+
+ namespace :admin do
+    resources :order_details, only:[:update]
+  end
+  namespace :admin do
+    resources :orders, only:[:show,:update]
+  end
+  namespace :admin do
+    resources :customers, only:[:index,:show,:edit,:update]
+  end
+  namespace :admin do
+    resources :genres, only:[:index,:create,:edit,:update]
+  end
+  namespace :admin do
+    resources :items, only:[:new,:index,:create,:show,:edit,:update]
+  end
 
 root to: "homes#top"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
