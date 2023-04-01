5585e166d1db147a1344567c4d7f02169e7b3fd7
ooishiho <yuidacheng697@gmail.com>
2023-04-01 04:33:05 +0000

    [add]public controller

---
+++ app/helpers/public/ragistrations_helper.rb
@@ -0,0 +1,2 @@
+module Public::RagistrationsHelper
+end
---
+++ test/controllers/public/ragistrations_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class Public::RagistrationsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
