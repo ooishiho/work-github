68ff20ce6bde7eab08a741ffc5f7bdb8d20377a6
ooishiho <yuidacheng697@gmail.com>
2023-04-03 14:37:20 +0000

    [fix]sign_up view

--- db/schema.rb
+++ db/schema.rb
@@ -82,7 +82,7 @@ ActiveRecord::Schema.define(version: 2023_04_01_065414) do
     t.string "first_name_kana", null: false
     t.string "postal_code", null: false
     t.string "address", null: false
-    t.string "telephone_number", null: false
+    t.string "telephon_nunber", null: false
     t.boolean "is_deleted", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
