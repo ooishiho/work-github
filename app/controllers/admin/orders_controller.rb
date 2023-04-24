bea4d69d2ca2a07d3fb157232c037b435921eec4
ooishiho <yuidacheng697@gmail.com>
2023-04-24 13:20:46 +0000

    [fix]adimn

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,10 +1,18 @@
 class Admin::OrdersController < ApplicationController
   def show
-   
-   @order.making_status = params[:order][:making_status]
+   @order = Order.find(params[:id])
+   @customer = @order.customer
+   @order_details = @order.order_details
+  # @order.making_status = params[:order][:making_status]
   end
 
   def update
-    
+
   end
+
+  private
+
+   def order_params
+   params.require(:order).permit(:payment_method,:shipping_cost,:total_payment, :postal_code, :address, :name)
+   end
 end
