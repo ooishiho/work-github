943edd852152f1e1ca9943297f50f3c2319ff661
ooishiho <yuidacheng697@gmail.com>
2023-04-29 12:28:53 +0000

    [add]status

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -3,6 +3,19 @@ class Admin::OrderDetailsController < ApplicationController
    @order_details = OrderDetail.find(params[:id])
    @order_details.update(order_params)
    @order = @order_details.order
+   if @order_details.making_status ==  "in_production"
+    @order.update(status: "in_production")
+   else
+     count = 0
+     @order_details.order.order_details.each do |order_detail|
+    if order_detail.making_status == "production_completed"
+       count += 1
+    end
+   end
+   if count == @order_details.order.order_details.count
+      @order.update(status: "preparing_to_ship")
+   end
+   end
    redirect_to admin_order_path(@order.id)
   end
 
